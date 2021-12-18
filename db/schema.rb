4bec0f8b5bc681badd0eb5788e77be37d700e2bc
yurlov43 <yurlov43@gmail.com>
2021-12-19 02:18:45 +0300

    Migration applied add_questions_body_null_constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_231349) do
+ActiveRecord::Schema.define(version: 2021_12_18_231655) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_12_18_231349) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
