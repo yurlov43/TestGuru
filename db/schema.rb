2d0f4c498e7ed7ecb6ffb9851fd1b70e7d0379fb
yurlov43 <yurlov43@gmail.com>
2021-12-19 02:41:40 +0300

    Migration applied add_answers_correct_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_232927) do
+ActiveRecord::Schema.define(version: 2021_12_18_233541) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "correct"
   end
 
   create_table "categories", force: :cascade do |t|
