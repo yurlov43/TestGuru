fae725f5da5d2904f37d9d173c887c7d4f488ec6
yurlov43 <yurlov43@gmail.com>
2021-12-19 02:22:21 +0300

    Migration applied add_categories_title_null_constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_231655) do
+ActiveRecord::Schema.define(version: 2021_12_18_231942) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_12_18_231655) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
