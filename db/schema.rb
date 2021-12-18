882fc9f89301fbd3830f4e23642dd44e1cbc2ca5
yurlov43 <yurlov43@gmail.com>
2021-12-19 02:15:50 +0300

    Migration applied add_users_name_null_constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_230703) do
+ActiveRecord::Schema.define(version: 2021_12_18_231349) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_12_18_230703) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
