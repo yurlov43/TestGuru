1148275cd0dec8e94064cbc4702421446a01806f
yurlov43 <yurlov43@gmail.com>
2021-12-19 02:45:41 +0300

    Migration applied add_answers_correct_default_value

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_233541) do
+ActiveRecord::Schema.define(version: 2021_12_18_234226) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "correct"
+    t.boolean "correct", default: false
   end
 
   create_table "categories", force: :cascade do |t|
