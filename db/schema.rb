559862812fb9e643c7d8cd93ebe600b7eda1b3d0
yurlov43 <yurlov43@gmail.com>
2021-12-20 01:45:47 +0300

    Migration applied add_from_tests_to_categories_foreign_key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_223820) do
+ActiveRecord::Schema.define(version: 2021_12_19_224338) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_12_19_223820) do
     t.integer "level", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +49,5 @@ ActiveRecord::Schema.define(version: 2021_12_19_223820) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
 end
